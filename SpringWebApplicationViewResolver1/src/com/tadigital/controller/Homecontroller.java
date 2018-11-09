4bf5d708b350d3caf5e2cbb7393dbde919509be6
Avleen Kapoor <avleen.k@tadigital.com>
2018-11-09 13:44:13 +0530

    Added source code for SpringWebApplication for View Resolver

---
+++ SpringWebApplicationViewResolver1/src/com/tadigital/controller/Homecontroller.java
@@ -0,0 +1,12 @@
+package com.tadigital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Homecontroller {
+	@RequestMapping(value = { "/" })
+	public String reloadHome() {
+		return "index";
+	}
+}
